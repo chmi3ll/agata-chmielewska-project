725c06a67752448c5c22caf17f294e96bb9ada20
Agata <chmielewska.agata33@gmail.com>
2018-07-14 11:08:46 +0200

    19.2 Ograniczenie skanowania w Swaggerze do pakietu

--- src/main/java/com/crud/tasks/config/CoreConfiguration.java
+++ src/main/java/com/crud/tasks/config/CoreConfiguration.java
@@ -1,26 +1,20 @@
 package com.crud.tasks.config;
 
 
-import com.google.common.base.Predicate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import static com.google.common.base.Predicates.and;
-import static springfox.documentation.builders.PathSelectors.regex;
-
 @EnableSwagger2
 @Configuration
 public class CoreConfiguration implements WebMvcConfigurer {
-
-    private static final String NO_ERROR_REGEX = "(?!.*error).*$";
-    private static final String NO_TRELLO_REGEX = "(?!.*trello).*$";
 
     @Bean
     public RestTemplate restTemplate() {
@@ -31,15 +25,9 @@ public class CoreConfiguration implements WebMvcConfigurer {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(paths())
+                .apis(RequestHandlerSelectors.basePackage("com.crud.tasks.controller"))
+                .paths(PathSelectors.any())
                 .build();
-    }
-
-    private Predicate<String> paths() {
-        return and(
-                regex(NO_ERROR_REGEX), regex(NO_TRELLO_REGEX)
-        );
     }
 
     @Override
